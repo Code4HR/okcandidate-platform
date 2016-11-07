ef208957dbbf60d6468cc665079465d115b7df71
Blaine Price <1wbprice@gmail.com>
2016-11-07 14:37:24 -0500

    set up postgres.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # okcandidate-platform
 A comprehensive refactor of [OKCandidate](https://github.com/Code4HR/okcandidate/) based on feedback from users and our partners at the Virginian-Pilot.
+
+# Setup
+```
+git clone git@github.com:Code4HR/okcandidate-platform.git && cd okcandidate-platform
+npm install
+npm start
+```
+
+# Contributing
+This project follows a loose interpretation of Git flow. Master is production, develop is staging. Each feature gets one branch.  When opening a PR, please open it against the develop branch.
--- config/database.js
+++ config/database.js
@@ -6,6 +6,12 @@
  *
  * @see {@link http://trailsjs.io/doc/config/database}
  */
+
+const dbName = process.env.PG_DATABASE_NAME || 'okcandidate_platform_dev'
+const dbUser = process.env.PG_DATABASE_USER || 'blaine'
+const dbPass = process.env.PG_DATABASE_PASS || ''
+const dbHost = process.env.PG_DATABASE_HOST || 'localhost'
+
 module.exports = {
 
   /**
@@ -15,23 +21,25 @@ module.exports = {
    *
    * Set production connection info in config/env/production.js
    */
+
   stores: {
 
-    /**
-     * Define a store called "local" which uses SQLite3 to persist data.
-     */
-    /*
-    sqlitedev: {
-      database: 'dev',
-      storage: './.tmp/dev.sqlite',
-      host: '127.0.0.1',
-      dialect: 'sqlite'
+    postgres: {
+      dialect: 'postgres',
+      database: dbName,
+      host: dbHost,
+      username: dbUser,
+      password: dbPass,
+      port: 5432,
+      ssl: false,
+      migrate: 'alter'
     }
-    */
+
   },
 
   models: {
-    defaultStore: 'sqlitedev',
+    defaultStore: 'postgres',
     migrate: 'alter'
   }
+
 }
