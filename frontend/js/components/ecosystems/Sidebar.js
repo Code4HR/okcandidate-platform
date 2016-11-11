2060f64043e0726df9c5253781295d441ffdc85d
Blaine Price <1wbprice@gmail.com>
2016-11-11 13:41:59 -0500

    Consolidate admin and user menus into one component that accepts a menu prop.

--- frontend/js/components/ecosystems/Sidebar.js
+++ frontend/js/components/ecosystems/Sidebar.js
@@ -1,17 +1,79 @@
 import React, { PropTypes, Component } from 'react'
 
 import AppLogo from './../organisms/AppLogo'
-import AdminMenu from './../ecosystems/AdminMenu'
-import UserMenu from './../ecosystems/UserMenu'
+import SidebarMenu from './../ecosystems/SidebarMenu'
 
 import {
   toggleSidebarVisibility
 } from './../../redux/actions/ui-actions' 
 
+const adminMenu = [
+  {
+    label: 'Survey',
+    submenu: [
+      {
+        label: 'New Survey',
+        icon: 'add',
+        href: '/admin/survey/new'
+      },
+      {
+        label: 'Edit Survey',
+        icon: 'edit',
+        href: '/admin/survey/'
+      },
+      {
+
+        label: 'Publish Survey',
+        icon: 'publish',
+        href: '/admin/surveys/'
+      },
+      {
+        label: 'Analyze Survey',
+        icon: 'trending_up',
+        href: '/admin/users/'
+      }
+    ]
+  },
+  {
+    label: 'Users',
+    submenu: [
+      {
+        label: 'Manage Users',
+        icon: 'people',
+        href: '/admin/users/'
+      }
+    ]
+  }
+]
+
+const userMenu = [
+  {
+    label: 'Survey',
+    submenu: [
+      {
+        label: 'Take Survey',
+        icon: 'check_circle',
+        href: '/admin/survey/new'
+      }
+    ]
+  }
+]
+
 class Sidebar extends Component {
 
   toggleSidebarVisibility() {
     this.props.dispatch(toggleSidebarVisibility())
+  }
+
+  getMenu() {
+    switch(this.props.role) {
+      case 'admin':
+        return adminMenu
+      case 'user':
+        return userMenu
+      default: 
+        return userMenu
+    }
   }
 
   render() {
@@ -33,8 +95,7 @@ class Sidebar extends Component {
           <AppLogo onClick={this.toggleSidebarVisibility.bind(this)} />
         </div>
 
-        {this.props.role === 'admin' && <AdminMenu />}
-        {this.props.role === 'user' && <UserMenu />}
+        <SidebarMenu menu={this.getMenu.call(this)} />
 
       </section>
     )
