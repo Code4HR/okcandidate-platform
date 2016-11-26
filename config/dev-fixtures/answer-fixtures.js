97ea2922de0f96e8e123da56cc46008275e4f25e
Blaine Price <1wbprice@gmail.com>
2016-11-26 15:56:35 -0500

    Add fixture files for questions and answers.

---
+++ config/dev-fixtures/answer-fixtures.js
@@ -0,0 +1,47 @@
+const answers = [
+  {
+    text: 'Not enough!'
+  },
+  {
+    text: 'Just right!'
+  },
+  {
+    text: 'Too much!'
+  }
+]
+
+module.exports = {
+
+  load: (app) => {
+    return app.orm.Answer.count({})
+    .then(count => {
+      debugger
+      if (count > 0) {
+        return
+      }
+      else {
+        // For all questions, create three answers.
+        app.orm.Question.findAll({where: {}})
+        .then(questions => {
+
+          return Promise.all(
+            questions.map((question, index) => {
+              return Promise.all(
+                answers.map((answer, index) => {
+                  return app.orm.Answer.create(
+                    Object.assign({}, answer, {QuestionId: question.toJSON().id})
+                  )
+                })
+              )
+            })
+          )
+        })
+        .then(questions => {
+          app.log.info('Answers created.')
+          return questions
+        })
+      }
+    })
+  }
+
+}
