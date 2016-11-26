23dbb829da02cdcbf9646103f42f657ec961794a
ttavenner <tommy.tavenner@gmail.com>
2016-11-26 17:14:15 -0500

    add region associations. resolves #32

--- api/models/Region.js
+++ api/models/Region.js
@@ -9,11 +9,29 @@ const Model = require('trails-model')
 module.exports = class Region extends Model {
 
   static config (app, Sequelize) {
-
+    return {
+      options: {
+        classMethods: {
+          associate: (models) => {
+            models.Region.belongsToMany(models.Survey, {through: 'surveyregion'}),
+            models.Region.belongsToMany(models.Office, {through: 'officeregion'}),
+            models.Region.hasMany(models.Region, {
+              onDelete: 'CASCADE',
+              foreignKey: 'parentRegionId'
+            })
+          }
+        }
+      }
+    }
   }
 
   static schema (app, Sequelize) {
-
+    return {
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    }
   }
 
 }
