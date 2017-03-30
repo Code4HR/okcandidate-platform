30b6905cd67985765f427c4560d51934e6daecc4
Blaine Price <1wbprice@gmail.com>
2017-03-29 20:03:11 -0400

    Match component. Add several match cards and minimal bottom margin to card atom.

--- frontend/js/components/environments/Results.js
+++ frontend/js/components/environments/Results.js
@@ -1,12 +1,19 @@
 'use strict';
 
 import React, { PropTypes, Component } from 'react';
+import Match from './../ecosystems/Match';
 
 class Results extends Component {
     render () {
         return (
             <article>
                 <pre>Results Page</pre>
+                <Match />
+                <Match />
+                <Match />
+                <Match />
+                <Match />
+                <Match />
             </article>
         );
     }
