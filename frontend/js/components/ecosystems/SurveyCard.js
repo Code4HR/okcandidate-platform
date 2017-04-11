15377409e697355f44500c3fc19a16e808ae0095
Blaine Price <1wbprice@gmail.com>
2017-04-11 18:58:42 -0400

    Rough out survey card.

---
+++ frontend/js/components/ecosystems/SurveyCard.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React, { PropTypes, Component } from 'react';
+
+import Card from './../atoms/Card';
+
+class SurveyCard extends Component {
+    render() {
+        return (
+            <Card>
+                <pre>SurveyCard</pre>
+            </Card>
+        );
+    }
+}
+
+SurveyCard.propTypes = {
+
+};
+
+module.exports = SurveyCard;
--- frontend/js/components/environments/Survey.js
+++ frontend/js/components/environments/Survey.js
@@ -1,12 +1,29 @@
 'use strict';
 
 import React, { PropTypes, Component } from 'react';
+import SurveyCard from './../ecosystems/SurveyCard';
 
-class Survey extends Component { 
+class Survey extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            survey: [
+                {},
+                {},
+                {}
+            ]
+        };
+    }
+
     render() {
         return (
             <article>
                 <pre>Survey Page</pre>
+                {
+                    this.state.survey.map((question, index) => {
+                        return <SurveyCard key={index}/>;
+                    })
+                }
             </article>
         );
     }
