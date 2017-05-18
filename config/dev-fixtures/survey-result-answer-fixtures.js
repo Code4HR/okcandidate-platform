c7c9bae8e61ff84badd7485ad00719cd6d112a33
ttavenner <tommy.tavenner@gmail.com>
2017-05-17 22:09:06 -0400

    update fixture data to support results page

--- config/dev-fixtures/survey-result-answer-fixtures.js
+++ config/dev-fixtures/survey-result-answer-fixtures.js
@@ -1,41 +1,118 @@
-'use strict'
+'use strict';
 
 const surveyResultAnswer = [
-  { intensity: 3, QuestionId: 1 },
-  { intensity: 3, QuestionId: 2 },
-  { intensity: 3, QuestionId: 3 }
-]
+    {
+        intensity: 3,
+        SurveyResultId: 1,
+        QuestionId: 1
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 1,
+        QuestionId: 2
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 1,
+        QuestionId: 3
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 2,
+        QuestionId: 1
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 2,
+        QuestionId: 2
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 2,
+        QuestionId: 3
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 3,
+        QuestionId: 1
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 3,
+        QuestionId: 2
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 3,
+        QuestionId: 3
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 4,
+        QuestionId: 1
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 4,
+        QuestionId: 2
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 4,
+        QuestionId: 3
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 5,
+        QuestionId: 1
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 5,
+        QuestionId: 2
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 5,
+        QuestionId: 3
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 6,
+        QuestionId: 1
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 6,
+        QuestionId: 2
+    },
+    {
+        intensity: 3,
+        SurveyResultId: 6,
+        QuestionId: 3
+    }
+];
 
 module.exports = {
-  load: (app) => {
+    load: (app) => {
 
-    return app.orm.SurveyResultAnswer.count({})
-    .then(count => {
-      if (count > 0) {
-        return []
-      }
-      else {
-        // Create Survey Result Answers
-        return app.orm.SurveyResult.findAll({where: {}})
-        .then(surveyResults => {
-          return Promise.all(
-            surveyResults.map(surveyResult => {
-              return Promise.all(
-                surveyResultAnswer.map(surveyResultAnswer => {
-                  return app.orm.SurveyResultAnswer.create(
-                    Object.assign({}, surveyResultAnswer, {SurveyResultId: surveyResult.id})
-                  )
-                })
-              )
-            })
-          )
-        })
-        .then((surveyResultAnswers) => {
-          app.log.info('Survey Result Answers created')
-          return surveyResultAnswers
-        })
-      }
-    })
-
-  }
-}
+        return app.orm.SurveyResultAnswer.count({})
+        .then(count => {
+            if (count > 0) {
+                return [];
+            }
+            else {
+                // Create Survey Result Answers
+                return Promise.all(
+                  surveyResultAnswer.map(surveyResultAnswer => {
+                      return app.orm.SurveyResultAnswer.create(surveyResultAnswer);
+                  })
+                )
+                .then((surveyResultAnswers) => {
+                    app.log.info('Survey Result Answers created');
+                    return surveyResultAnswers;
+                });
+            }
+        });
+    }
+};
