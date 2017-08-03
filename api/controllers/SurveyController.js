90dbb464c964b0325be004f3951fb014f5903870
ttavenner <tommy.tavenner@gmail.com>
2017-08-03 18:37:49 -0400

    remove update function on SurveyController

--- api/controllers/SurveyController.js
+++ api/controllers/SurveyController.js
@@ -20,17 +20,6 @@ module.exports = class SurveyController extends Controller {
         });
     }
 
-    update(request, reply) {
-        const record = request.payload;
-        return this.app.orm.Survey.update(record, {where: {id: record.id}})
-        .then((record) => {
-            return reply(record);
-        })
-        .catch((error) => {
-            return reply(Boom.badRequest(error));
-        });
-    }
-
     getSurveyByLatLng(request, reply) {
 
         const coordinates = JSON.parse(request.query.coordinates);
