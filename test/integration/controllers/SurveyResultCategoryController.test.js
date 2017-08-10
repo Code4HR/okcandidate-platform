24f6d877181c89dee5f05b715c11fe002d8b14cd
Blaine Price <1wbprice@gmail.com>
2017-08-10 14:42:52 -0400

    Remove more stuff.

--- test/integration/controllers/SurveyResultCategoryController.test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const assert = require('assert')
-
-describe('SurveyResultCategoryController', () => {
-  it('should exist', () => {
-    assert(global.app.controllers.SurveyResultCategoryController)
-  })
-})
