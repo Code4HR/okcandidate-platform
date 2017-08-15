51ad24f06cb4f0122ca820220bd0a18c497dcd77
Blaine Price <1wbprice@gmail.com>
2017-08-15 15:49:57 -0400

    Sort candidates in decreasing order

--- frontend/js/components/environments/Results.js
+++ frontend/js/components/environments/Results.js
@@ -23,6 +23,16 @@ class Results extends Component {
         this.props.dispatch(fetchSurveyResults(this.props.params.passPhrase));
     }
 
+    sortMatches(matchA, matchB) {
+        if (matchA.matchRate < matchB.matchRate) {
+            return 1;
+        }
+        else if (matchB.matchRate < matchA.MatchRate) {
+            return -1;
+        }
+        return 0;
+    }
+
     render () {
 
         const matches = this.props.result.matches;
@@ -35,9 +45,11 @@ class Results extends Component {
             return o.matchRate == bestRate;
         });
 
-        const otherMatches = matches.filter((o) => {
-            return o.matchRate != bestRate;
-        });
+        const otherMatches = matches
+            .filter((o) => {
+                return o.matchRate != bestRate;
+            })
+            .sort(this.sortMatches);
 
         return (
             <article className="container">
@@ -50,21 +62,23 @@ class Results extends Component {
 
                 { bestMatch.map((m) => {
                     return (<BestMatch
-                            key={m.name}
-                            matchText={bestMatch.length > 1 ?
-                              "It's a duplicate!" :
-                              "It's a match!"}
-                            name={m.name}
-                            office={m.office}
-                            matchRate={Math.round(m.matchRate)} />);
+                        key={m.name}
+                        matchText={bestMatch.length > 1 ?
+                            "It's a duplicate!" :
+                            "It's a match!"}
+                        name={m.name}
+                        party="Party Affiliation"
+                        office={m.office}
+                        matchRate={Math.round(m.matchRate)} />);
                 })}
 
                 { otherMatches.map((m) => {
                     return (<BestMatch
-                            key={m.name}
-                            name={m.name}
-                            office={m.office}
-                            matchRate={Math.round(m.matchRate)} />);
+                        key={m.name}
+                        name={m.name}
+                        party="Party Affiliation"
+                        office={m.office}
+                        matchRate={Math.round(m.matchRate)} />);
                 })}
 
                 <ElectionReminderPrompt />
@@ -81,9 +95,9 @@ Results.propTypes = {
 };
 
 export default connect(
-  state => ({
-      ui: state.ui,
-      login: state.login,
-      result: state.result
-  })
+    state => ({
+        ui: state.ui,
+        login: state.login,
+        result: state.result
+    })
 )(Results);
