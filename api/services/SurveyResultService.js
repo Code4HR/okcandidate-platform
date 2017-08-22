cec212f51421a705ba880dd1ded59214eb7c807d
Tommy Tavenner <tommy.tavenner@gmail.com>
2017-08-22 19:04:55 -0400

    Return match rate by category in results service. Resolves #211 (#238)

--- api/services/SurveyResultService.js
+++ api/services/SurveyResultService.js
@@ -23,40 +23,50 @@ module.exports = class SurveyResultService extends Service {
         })
         .then((result) => {
             const query = `select surveyresult.id as "SurveyResultId"
-                , questiontype."multipleChoice"
-                , questiontype.sentiment
-                , surveyresultanswer."QuestionId"
-                , surveyresultanswer."AnswerId"
-                , surveyresultanswer.sentiment as "SurveyResultSentiment"
-                , candidate."CandidateId"
-                , candidate."Name"
-                , candidate."Office"
-                , candidate."Party"
-                , candidate."AnswerId" as "CandidateAnswerId"
-                , candidate.sentiment as "CandidateSentiment"
-                from surveyresult
-                inner join survey 
-		          on survey.id = surveyresult."SurveyId"
-		        inner join questiontype
-		          on questiontype.id = survey."QuestionTypeId"
-                inner join surveyresultanswer
-                  on surveyresultanswer."SurveyResultId" = surveyresult.id
-                left outer join (select t0.id as "CandidateId"
-                                  , t0.name as "Name"
-                                  , t1.name as "Office"
-                                  , t0.party as "Party"
-                                  , t3."QuestionId"
-                                  , t3."AnswerId"
-                                  , t3.sentiment
-                                  from candidate t0
-                                  inner join office t1
-                                    on t1.id = t0."OfficeId"
-                                  inner join surveyresult t2
-                                    on t2."CandidateId" = t0.id
-                                  inner join surveyresultanswer t3
-                                    on t3."SurveyResultId" = t2.id) candidate
-                  on candidate."QuestionId" = surveyresultanswer."QuestionId"
-                where surveyresult.id = ?`;
+            , survey."categorySort"
+            , questiontype."multipleChoice"
+            , questiontype.sentiment
+            , question."CategoryId"
+            , category.name as "CategoryName"
+            , surveyresultcategory.rank as "CategoryRank"
+            , surveyresultanswer."QuestionId"
+            , surveyresultanswer."AnswerId"
+            , surveyresultanswer.sentiment as "SurveyResultSentiment"
+            , candidate."CandidateId"
+            , candidate."Name"
+            , candidate."Office"
+            , candidate."Party"
+            , candidate."AnswerId" as "CandidateAnswerId"
+            , candidate.sentiment as "CandidateSentiment"
+            from surveyresult
+            inner join survey 
+              on survey.id = surveyresult."SurveyId"
+            inner join questiontype
+              on questiontype.id = survey."QuestionTypeId"
+            inner join surveyresultanswer
+              on surveyresultanswer."SurveyResultId" = surveyresult.id
+            inner join question
+              on question.id = surveyresultanswer."QuestionId"
+            left outer join category
+              on category.id = question."CategoryId"
+            left outer join surveyresultcategory
+              on surveyresultcategory."CategoryId" = question."CategoryId"
+            left outer join (select t0.id as "CandidateId"
+                              , t0.name as "Name"
+                              , t1.name as "Office"
+                              , t0.party as "Party"
+                              , t3."QuestionId"
+                              , t3."AnswerId"
+                              , t3.sentiment
+                              from candidate t0
+                              inner join office t1
+                                on t1.id = t0."OfficeId"
+                              inner join surveyresult t2
+                                on t2."CandidateId" = t0.id
+                              inner join surveyresultanswer t3
+                                on t3."SurveyResultId" = t2.id) candidate
+              on candidate."QuestionId" = surveyresultanswer."QuestionId"
+            where surveyresult.id = ?`;
 
             return this.app.orm.SurveyResultAnswer.sequelize.query(query, {
                 replacements: [result.id],
@@ -87,18 +97,40 @@ module.exports = class SurveyResultService extends Service {
                         office: v.Office,
                         party: v.Party,
                         score: score,
-                        numQuestions: 1
+                        numQuestions: 1,
+                        categories: []
+                    };
+
+                    const category = {
+                        categoryName: v.CategoryName,
+                        numQuestions: 1,
+                        score: score
                     };
 
                     const existing = a.findIndex(e =>
                         (e.name == candidate.name && e.office == candidate.office));
 
                     if (existing >= 0) {
+
                         a[existing].score += candidate.score;
                         a[existing].numQuestions += candidate.numQuestions;
+
+                        const existingCat = a[existing].categories.findIndex(e =>
+                            (e.categoryName == category.categoryName));
+
+                        if (existingCat >= 0) {
+                            a[existing].categories[existingCat].numQuestions += category.numQuestions;
+                            a[existing].categories[existingCat].score += category.score;
+                        }
+
+                        else {
+                            a[existing].categories.push(category);
+                        }
+
                         return a;
                     }
 
+                    candidate.categories.push(category);
                     return [candidate, ...a];
                 }
 
@@ -107,6 +139,10 @@ module.exports = class SurveyResultService extends Service {
 
             return matches.map(m => {
                 m.matchRate = (m.score / m.numQuestions) * 100;
+                m.categories = m.categories.map((c) => {
+                    c.matchRate = (c.score == 0 ? 0 : (c.score / c.numQuestions) * 100);
+                    return c;
+                });
                 return m;
             });
         });
